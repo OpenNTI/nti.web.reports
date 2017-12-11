6e7776a8ac5012818ce8f752b9c660c44d2a67cd
Andrew Ligon <andrew.ligon@nextthought.com>
2017-12-11 13:23:34 -0600

    Initial Commit

---
+++ src/View.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TestCmp () {
+	return (
+		<span>
+			Hello World
+		</span>
+	);
+}
---
+++ test/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import View from '../../src';
+
+ReactDOM.render(
+	React.createElement(View, {}),
+	document.getElementById('content')
+);
