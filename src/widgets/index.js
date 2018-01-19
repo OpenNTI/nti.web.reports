6738f8e8194316e31a83b1cf152f0f9e7a44fbe2
Josh Birdwell <jbirdwell34@gmail.com>
2018-01-19 15:08:51 -0600

    Forgot to export a widget

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -2,4 +2,5 @@ export ActiveDays from './ActiveDays';
 export ActiveUsers from './ActiveUsers';
 export ActiveTimes from './ActiveTimes';
 export LabeledValue from './LabeledValue';
+export ActiveTimesChart from './ActiveTimesChart';
 export Card from './Card';
