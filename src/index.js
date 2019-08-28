4a8a4d7c92d8969c60988535ba8ad03475251347
Andrew Ligon <andrew.ligon@nextthought.com>
2019-08-28 10:48:47 -0500

    expose a common launch button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export * as Widgets from './widgets';
+export * as Launch from './launch';
 export List from './list';
 export Viewer from './viewer';
---
+++ src/launch/Button.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
+
+import View from '../viewer';
+
+import Styles from './Button.css';
+
+const cx = classnames.bind(Styles);
+
+export default class ReportLaunchButton extends React.Component {
+	static propTypes = {
+		className: PropTypes.string,
+		report: PropTypes.object
+	}
+
+	launchReport = (e) => {
+		e.stopPropagation();
+		e.preventDefault();
+
+		const {report} = this.props;
+
+		View.show(report);
+	}
+
+	render () {
+		const {className, report} = this.props;
+
+		if (!report) { return null; }
+
+		return (
+			<button type="button" className={cx('report-launch-button', className)} onClick={this.launchReport}>
+				<i className={cx('icon-report', 'icon')} />
+			</button>
+		);
+	}
+
+}
