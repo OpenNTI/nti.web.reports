6b9d4085700558cc07fb3ced5d08d50d2d5a7beb
Andrew Ligon <andrew.ligon@nextthought.com>
2020-06-05 14:16:18 -0500

    Remove web-discussions dependency

--- src/contexts/course-instance/forums/Store.js
+++ src/contexts/course-instance/forums/Store.js
@@ -1,5 +1,65 @@
 import {Stores} from '@nti/lib-store';
-import {binDiscussions} from '@nti/web-discussions';
+
+/**
+ * Takes two arrays of forums and bins then
+ *
+ *	1.) by for credit or open
+ *	2.) by if they are for this section or the parent
+ *
+ * returns an object that looks like:
+ *
+ *	{
+ *		ForCredit: {
+ *			Section: {id: String, items: Array[Forum]},
+ *			Parent: {id: String, items: Array[Forum]}
+ *		},
+ *		Open: {
+ *			Section: {id: String, items: Array[Forum]},
+ *			Parent: {id: String, items: Array[Forum]}
+ *		},
+ *		Other: ...(same as above)
+ *	}
+ *
+ * @param  {object} section Object of forums in this section
+ * @param  {object} parent  Object of forums in the parent if there are any
+ * @return {object}        The binned forums
+ */
+const binDiscussions = (section, parent) => {
+	let bins = {};
+
+	function addTo (key, group) {
+
+		let items = (group && group.Items) || [];
+		for (let item of items) {
+			let bin = item.getBin();
+			if (!bins[bin]) {
+				bins[bin] = {};
+			}
+
+			bin = bins[bin];
+			if (!bin[key]) {
+				bin[key] = { id: group.NTIID, forums: [] };
+			}
+
+			bin[key].forums.push(item);
+		}
+	}
+
+	addTo('Section', section);
+	addTo('Parent', parent);
+
+	// Stub out empty case
+	if (!section.Items || section.Items.length === 0) {
+		bins.Other = {
+			...bins.Other,
+			Section: {
+				id: section.NTIID
+			}
+		};
+	}
+
+	return bins;
+};
 
 export default class CourseForumsStore extends Stores.SimpleStore {
 	constructor () {
